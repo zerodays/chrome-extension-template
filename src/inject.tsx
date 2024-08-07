e258574c34cfcf3c1ccaa49bf955f0d56389cfd2
Domen Koščak <domen.koscak@zerodays.dev>
2024-08-07 08:05:32 +0200

    feat: shadow DOM

--- src/inject.tsx
+++ src/inject.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './globals.css';
+import css from './globals.css?inline';
 import './i18n/config';
 import { InjectContent } from './inject-content';
 
-// Inits the react root inside the content of a tab
+// Creates a host element in webpage body to attach shadow to
+const host = document.createElement('div');
+host.id = 'crx-host';
+document.body.appendChild(host);
+
+// Attaches shadow to host element
+const shadow = host.attachShadow({ mode: 'open' });
+
+// Injects global styles to shadow
+const style = new CSSStyleSheet();
+style.replaceSync(css);
+shadow.adoptedStyleSheets = [style];
+
+// Creates a root element to attach react to
 const root = document.createElement('div');
 root.id = 'crx-root';
-document.body.appendChild(root);
 
+// Attaches react to shadow
+shadow.appendChild(root);
+
+// Renders react in '#crx-root' element
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <InjectContent />
